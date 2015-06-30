956e0bc759e6f8bd960373c8e591fa84f3b6bf1e
Chris Scheller <schelcj@pobox.com>
2015-06-30 14:55:08 -0400

    run limited set of illumina jobs for testing

--- launch.sh
+++ launch.sh
@@ -4,16 +4,20 @@ INCOMING="/net/topmed/incoming/topmed"
 CENTERS=($(find $INCOMING -maxdepth 1))
 
 for center in "${CENTERS[@]}"; do
-  BAMS=($(find $center -name '*.bam' | head -n 200 | tail -n 100))
+  BAMS=($(find $center -name '*.bam' | head -n 100))
   BAM_CENTER=$(basename $center)
 
   if [ "$BAM_CENTER" == 'topmed' ] || [ "$BAM_CENTER" == 'illumina-upload' ]; then
     continue
   fi
 
-  for bam in "${BAMS[@]}"; do
-    export BAM_FILE=$bam
-    echo "$BAM_CENTER $BAM_FILE"
-    #sbatch ./align.sh
-  done
+  if [ "$BAM_CENTER" == 'illumina' ]; then
+    export BAM_CENTER=$BAM_CENTER
+
+    for bam in "${BAMS[@]}"; do
+      export BAM_FILE=$bam
+
+      sbatch ./align.sh
+    done
+  fi
 done
