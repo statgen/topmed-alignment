30d877525e61c758745a4a45e350047d80d2d86f
Chris Scheller <schelcj@pobox.com>
2015-07-17 14:51:19 -0400

    need to track the db id from the monitor to reference it in the cache

--- lib/perl5/Topmed/Command/update.pm
+++ lib/perl5/Topmed/Command/update.pm
@@ -2,22 +2,55 @@ package Topmed::Command::update;
 
 use Topmed -command;
 use Topmed::Base;
+use Topmed::Config;
 
 sub opt_spec {
   return (
-    ['verbose|v',    'Verbose output'],
-    ['bam|b=s',      'bam file to update'],
-    ['processing|p', 'mark the bam file as in process'],
-    ['finished|f',   'mark the bam file finished'],
+    ['bamid|b=i', 'ID From topmed db of bam to update'],
+    ['completed', 'mark the bam file finished'],
+    ['failed',    'mark the bam as failed'],
   );
 }
 
 sub validate_args {
   my ($self, $opts, $args) = @_;
+
+  my $conf  = Topmed::Config->new();
+  my $cache = $conf->cache();
+
+  unless ($opts->{bamid}) {
+    $self->usage_error('BAM DB ID is required');
+  }
+
+  my $entry = $cache->entry($opts->{bamid});
+
+  unless ($entry->exists) {
+    $self->usage_error('BAM does not exist in cache');
+  }
+
+  $self->{stash}->{cache_entry} = $entry;
+
+  if ($self->app->global_options->{help}) {
+    print $self->app->usage->text;
+    exit;
+  }
 }
 
 sub execute {
   my ($self, $opts, $args) = @_;
+
+  my $entry = $self->{stash}->{cache_entry};
+  my $bam   = $entry->thaw();
+
+  if ($opts->{failed}) {
+    $bam->{status} = $BAM_STATUS{failed};
+
+  } elsif ($opts->{completed}) {
+    $bam->{status} = $BAM_STATUS{completed};
+
+  }
+
+  $entry->freeze($bam);
 }
 
 1;
