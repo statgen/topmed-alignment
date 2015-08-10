8d894f2ae025370c31ea305a9bcacb5b032bfb5f
Chris Scheller <schelcj@pobox.com>
2015-08-10 14:07:14 -0400

    trying to detect failed jobs in flux to resubmit

--- scripts/resub.pl
+++ scripts/resub.pl
@@ -4,45 +4,36 @@ use Modern::Perl;
 use Data::Dumper;
 use List::MoreUtils qw(:all);
 use File::Slurp qw(read_file);
-use IPC::System::Simple qw(capture run);
+use IPC::System::Simple qw(capture);
 use Topmed::Config;
-use Topmed::DB;
 
-my $db    = Topmed::DB->new();
+my $clst  = $ARGV[0];
 my $conf  = Topmed::Config->new();
 my $cache = $conf->cache();
 my $index = $cache->entry($BAM_CACHE_INDEX)->thaw();
 
 for my $id (keys %{$index}) {
   my $bam = $cache->entry($id)->thaw();
-  next unless $bam->{status} == $BAM_STATUS{submitted};
+  next unless $bam->{status} == $BAM_STATUS{submitted} or $bam->{status} == $BAM_STATUS{failed};
 
   if (exists $bam->{clst} and exists $bam->{job_id}) {
-    next if $bam->{job_id} =~ /nyx/;
-    chomp(my $job_state = capture(qq(sacct -j $bam->{job_id} -X -n -o state%7)));
-
-    next if $job_state =~ /running/i;
-
-    say "bin/topmed resubmit -b $id";
+    given ($clst) {
+      when (/csg/) {_process_csg_jobs($bam->{job_id}, $id)}
+      when (/flux/) {_process_flux_jobs($bam->{job_id}, $id)}
+    }
   }
 }
 
-__END__
-my @jobs = apply {chomp $_} capture(q{squeue -u schelcj -n align-topmed -h -o %i});
-my @subs = apply {chomp $_} capture(q(awk {'print $2'} ../tmp/submitted.txt));
-for my $sub (@subs) {
-  my $bam       = $db->resultset('Bamfile')->find($sub);
-  my $bam_cache = $cache->entry($sub)->thaw();
+sub _process_csg_jobs {
+  my ($job_id, $bam_id) = @_;
+  chomp(my $job_state = capture(qq(sacct -j $job_id -X -n -o state%7)));
+  next if $job_state =~ /running|pending/i;
+  say "bin/topmed resubmit -b $bam_id";
+}
 
-  if (exists $bam_cache->{clst} and exists $bam_cache->{job_id}) {
-    next if $bam_cache->{job_id} =~ /nyx/;
-    my $job_state = capture(qq(sacct -j $bam_cache->{job_id} -X -n -o state%7));
-
-    unless ($job_state =~ /RUNNING/) {
-    }
-
-    if ($job_state =~ /CANCEL/) {
-      say "bin/topmed resubmit -b $sub -v ";
-    }
-  }
+sub _process_flux_jobs {
+  my ($job_id, $bam_id) = @_;
+  my $rc = system(qq{checkjob -v $job_id 2>&1 > /dev/null && echo \$?});
+  return unless $rc;
+  say "bin/topmed resubmit -b $bam_id";
 }
