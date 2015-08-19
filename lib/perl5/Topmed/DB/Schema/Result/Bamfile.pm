b32a541e2b2319376abfff47ce7561220d6adb71
Chris Scheller <schelcj@pobox.com>
2015-08-19 11:00:00 -0400

    must have ran perltidy against this at some point which hoses schema loader

--- lib/perl5/Topmed/DB/Schema/Result/Bamfile.pm
+++ lib/perl5/Topmed/DB/Schema/Result/Bamfile.pm
@@ -1,5 +1,4 @@
 use utf8;
-
 package Topmed::DB::Schema::Result::Bamfile;
 
 # Created by DBIx::Class::Schema::Loader
@@ -57,6 +56,12 @@ __PACKAGE__->table("bamfiles");
   data_type: 'varchar'
   is_nullable: 1
   size: 96
+
+=head2 nwdid
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 32
 
 =head2 checksum
 
@@ -188,33 +193,62 @@ __PACKAGE__->table("bamfiles");
 =cut
 
 __PACKAGE__->add_columns(
-  "bamid", {data_type => "integer", is_auto_increment => 1, is_nullable => 0},
-  "runid", {data_type => "integer", is_nullable => 0},
-  "studyid",       {data_type => "integer", is_nullable   => 0},
-  "bamname",       {data_type => "varchar", is_nullable   => 0, size => 96},
-  "studyname",     {data_type => "varchar", is_nullable   => 0, size => 96},
-  "piname",        {data_type => "varchar", is_nullable   => 1, size => 96},
-  "checksum",      {data_type => "varchar", is_nullable   => 0, size => 96},
-  "refname",       {data_type => "varchar", is_nullable   => 0, size => 96},
-  "expt_refname",  {data_type => "varchar", is_nullable   => 0, size => 96},
-  "expt_sampleid", {data_type => "varchar", is_nullable   => 0, size => 24},
-  "datearrived",   {data_type => "varchar", is_nullable   => 1, size => 12},
-  "datemd5ver",    {data_type => "varchar", is_nullable   => 1, size => 12},
-  "dateqplot",     {data_type => "varchar", is_nullable   => 1, size => 12},
-  "datemapping",   {data_type => "varchar", is_nullable   => 1, size => 12},
-  "datereport",    {data_type => "varchar", is_nullable   => 1, size => 12},
-  "datebackup",    {data_type => "varchar", is_nullable   => 1, size => 12},
-  "datebai",       {data_type => "varchar", is_nullable   => 1, size => 12},
-  "datecp2ncbi",   {data_type => "varchar", is_nullable   => 1, size => 12},
-  "jobidarrived",  {data_type => "varchar", is_nullable   => 1, size => 12},
-  "jobidmd5ver",   {data_type => "varchar", is_nullable   => 1, size => 12},
-  "jobidbackup",   {data_type => "varchar", is_nullable   => 1, size => 12},
-  "jobidbai",      {data_type => "varchar", is_nullable   => 1, size => 12},
-  "jobidqplot",    {data_type => "varchar", is_nullable   => 1, size => 12},
-  "jobidmapping",  {data_type => "varchar", is_nullable   => 1, size => 12},
-  "jobidcp2ncbi",  {data_type => "varchar", is_nullable   => 1, size => 12},
-  "bamsize",       {data_type => "varchar", default_value => 0, is_nullable => 1, size => 16},
-  "dateinit", {data_type => "varchar", is_nullable => 1, size => 10},
+  "bamid",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "runid",
+  { data_type => "integer", is_nullable => 0 },
+  "studyid",
+  { data_type => "integer", is_nullable => 0 },
+  "bamname",
+  { data_type => "varchar", is_nullable => 0, size => 96 },
+  "studyname",
+  { data_type => "varchar", is_nullable => 0, size => 96 },
+  "piname",
+  { data_type => "varchar", is_nullable => 1, size => 96 },
+  "nwdid",
+  { data_type => "varchar", is_nullable => 1, size => 32 },
+  "checksum",
+  { data_type => "varchar", is_nullable => 0, size => 96 },
+  "refname",
+  { data_type => "varchar", is_nullable => 0, size => 96 },
+  "expt_refname",
+  { data_type => "varchar", is_nullable => 0, size => 96 },
+  "expt_sampleid",
+  { data_type => "varchar", is_nullable => 0, size => 24 },
+  "datearrived",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "datemd5ver",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "dateqplot",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "datemapping",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "datereport",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "datebackup",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "datebai",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "datecp2ncbi",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "jobidarrived",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "jobidmd5ver",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "jobidbackup",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "jobidbai",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "jobidqplot",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "jobidmapping",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "jobidcp2ncbi",
+  { data_type => "varchar", is_nullable => 1, size => 12 },
+  "bamsize",
+  { data_type => "varchar", default_value => 0, is_nullable => 1, size => 16 },
+  "dateinit",
+  { data_type => "varchar", is_nullable => 1, size => 10 },
 );
 
 =head1 PRIMARY KEY
@@ -230,8 +264,8 @@ __PACKAGE__->add_columns(
 __PACKAGE__->set_primary_key("bamid");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07043 @ 2015-07-07 09:49:23
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:+6XUDVmMr83Ut0Ggm27suA
+# Created by DBIx::Class::Schema::Loader v0.07043 @ 2015-08-19 10:56:01
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:JpZYBI5to2uP5re4l41VjQ
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
