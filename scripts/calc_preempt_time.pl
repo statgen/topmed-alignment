fdb10727f31cc5526bba9e3c3c17e50725340a97
Chris Scheller <schelcj@pobox.com>
2015-08-24 14:36:04 -0400

    misc scripts to sort out some issues

---
+++ scripts/calc_preempt_time.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+
+use Topmed::Base;
+use Topmed::Config;
+use List::MoreUtils qw(minmax);
+
+my %jobs = ();
+my $total = 0;
+
+while (<STDIN>) {
+  chomp;
+  my ($jobid, $time) = split(/\s/);
+  my $hours = parse_time($time);
+  $jobs{$jobid} += $hours;
+  $total += $hours;
+}
+
+my ($min, $max) = minmax(values %jobs);
+say "Average Hours: " . $total / scalar keys %jobs;
+say "Max Hours: " . $max;
+say "Total Hours: $total";
+
+sub parse_time {
+  my ($time) = @_;
+
+  for my $regexp (@TIME_REMAINING_FORMAT_REGEXPS) {
+    if ($time =~ $regexp) {
+      return (($+{days} * 24) + $+{hours}) if $+{days} and $+{hours};
+      return $+{hours} if $+{hours};
+      return int($+{seconds} / 60 / 60) if $+{seconds};
+    }
+  }
+
+  return 0;
+}
+
+
