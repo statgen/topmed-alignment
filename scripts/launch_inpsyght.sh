cfd3db4d5e4afe722e3984be97ea666b8c286222
Chris Scheller <schelcj@pobox.com>
2015-10-12 08:31:45 -0400

    make it easier to sort out what is in the queue

--- scripts/launch_inpsyght.sh
+++ scripts/launch_inpsyght.sh
@@ -7,6 +7,8 @@ for bam in "${BAMS[@]}"; do
   export BAM_FILE=$bam
   export SAMPLE_ID="$(samtools view -H $bam| grep '^@RG' | grep -o 'SM:\S*' | sort -u | cut -d \: -f 2)"
 
+  job_name="inpsyght-${SAMPLE_ID}"
+
   echo "Submitting BAM: $BAM_FILE for alignment with SAMPLE_ID: $SAMPLE_ID with initial DELAY: $DELAY"
-  qsub -N $SAMPLE_ID batch.d/inpsyght.sh
+  qsub -N $job_name batch.d/inpsyght.sh
 done
