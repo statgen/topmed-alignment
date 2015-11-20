e3b436fab99cc30835973048568b3b655d7f2c1b
Chris Scheller <schelcj@pobox.com>
2015-11-20 08:05:54 -0500

    a few samples are giving me grief

--- scripts/launch_gecco.sh
+++ scripts/launch_gecco.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-BAMS=($(awk {'print $3'} /net/esp/saichen/allYears/allYears.bam.index))
-
-for bam in "${BAMS[@]}"; do
+for bam in $*; do
   export DELAY=$(($RANDOM % 120))
   export BAM_FILE=$bam
   export SAMPLE_ID="$(samtools view -H $bam| grep '^@RG' | grep -o 'SM:\S*' | sort -u | cut -d \: -f 2)"
 
-  if [ $SAMPLE_ID != '63305' ]; then
+  if [ $SAMPLE_ID -ne '63305' ]; then
+    job_name=gecco-${SAMPLE_ID}
     echo "Submitting BAM: $BAM_FILE for alignment with SAMPLE_ID: $SAMPLE_ID with initial DELAY: $DELAY"
-    sbatch -J $SAMPLE_ID batch.d/gecco.sh
+    sbatch -J $job_name batch.d/gecco.sh
   fi
 done
